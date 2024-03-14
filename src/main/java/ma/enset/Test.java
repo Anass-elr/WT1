3bbd01fe85dae8e8875c867bba624df2df59753a
Anass-elr <elrhazi199@gmail.com>
2024-03-14 15:47:36 +0000

    Comments

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,7 +1,11 @@
 package ma.enset;
 
+/*
+Comment pour modification
+ */
 public class Test {
     public static void main(String[]  args){
+        //---------------------------------
         System.out.println("Hello");
     }
 }
